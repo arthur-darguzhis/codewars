a28fa46a4dbe7b05661bd65527692c557b24e032
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-17 21:18:09 +0300

    sql 9 task https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/sql

---
+++ SQL/009.sql
@@ -0,0 +1,5 @@
+-- https://www.codewars.com/kata/590cc86f7557c0494000007e/train/sql
+INSERT INTO participants (name, age, attending) VALUES ('Arthur', 30, true); --Your code here
+
+SELECT * FROM participants;
+
