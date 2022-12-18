dc3d798310facb75a75405da321f53df4518220c
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-18 11:28:51 +0300

    add kyu7 10 task

---
+++ SQL/kyu7/10.sql
@@ -0,0 +1,4 @@
+-- https://www.codewars.com/kata/59414360f5c3947364000070/train/sql
+/*  SQL  */
+SELECT CONCAT(name, name, name) as name, REVERSE(characteristics) as characteristics
+FROM monsters;
