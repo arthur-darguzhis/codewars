0893bd6752e3793717164cf2f6b55ddfba5a0a4d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-18 12:27:44 +0300

    add kyu7 17 task

---
+++ SQL/kyu7/17.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/5abcf0f930488ff1a6000b66/train/sql
+/* Oh you may not think I'm pretty,
+But don't judge on what you see,
+I'll eat myself if you can find
+A smarter hat than me. */
+SELECT id, name, quality1, quality2
+FROM students
+WHERE (quality1 = 'evil' AND quality2 = 'cunning')
+   OR (quality1 = 'brave' AND quality2 != 'evil')
+   OR (quality1 = 'studious' OR quality2 = 'intelligent')
+   OR (quality1 = 'hufflepuff' OR quality2 = 'hufflepuff')
+ORDER BY id
