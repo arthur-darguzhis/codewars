7e45aeaf339a96e2e05c460714c4e42c9c0ff32d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-09 11:24:58 +0300

    add kyu8 38 task

---
+++ JavaScript/kyu8/Arrays/38.js
@@ -0,0 +1,5 @@
+const sortByLength = array => array.sort((a, b) => a.length - b.length)
+// Return an array containing the same strings, ordered from shortest to longest
+
+
+sortByLength(["Beg", "Life", "I", "To"]) //["I", "To", "Beg", "Life"];
